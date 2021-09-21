cfb3ba4a0e0dd3d46c5c632d6a34cfe0b44a8beb
Georgi Grigorov <72125420+Grigorov-Georgi@users.noreply.github.com>
2021-09-22 00:46:56 +0300

    Deleted false directory

--- JavaScript-Advanced/01.SyntaxFunctionsAndStatements/probe.js
+++
@@ -1,9 +0,0 @@
-var text = 'asd';
-console.log(`This is the needed text: ${text}`);
-
-var text1 = 2;
-
-
-let myInt = 1
-console.log(`This is the needed text ${myInt}`);
-console.log(`my variable is ${myInt}`);
