ba90ff221ea82182c605c21c655a21003d2ed187
Georgi Grigorov <72125420+Grigorov-Georgi@users.noreply.github.com>
2022-03-10 22:56:55 +0200

    Delete Spring_Data/_01_spring_data_lab directory

--- Spring_Data/_01_spring_data_lab/services/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.example._11_spring_data_lab.services;
-
-import java.math.BigDecimal;
-
-public interface AccountService {
-    void withdrawMoney(BigDecimal amount, Long id);
-
-    void depositMoney(BigDecimal amount, Long id);
-}
--- Spring_Data/_01_spring_data_lab/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example._11_spring_data_lab.services;
-
-import com.example._11_spring_data_lab.models.User;
-
-public interface UserService {
-    void register(User user);
-}
