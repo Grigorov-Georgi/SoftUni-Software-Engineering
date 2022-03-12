6685a87c4ea15e324849d58f7d90ea229f10f755
Georgi Grigorov <72125420+Grigorov-Georgi@users.noreply.github.com>
2022-03-12 20:47:39 +0200

    Delete Spring_Data/12_Spring_Data_Bookshop directory

--- Spring_Data/12_Spring_Data_Bookshop/src/main/java/com/example/demo/services/AuthorService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services;
-
-import com.example.demo.entities.Author;
-import org.springframework.stereotype.Service;
-
-
-public interface AuthorService  {
-    Author getRandomAuthor();
-}
--- Spring_Data/12_Spring_Data_Bookshop/src/main/java/com/example/demo/services/BookService.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.services;
-
-public interface BookService {
-    public void insert();
-}
